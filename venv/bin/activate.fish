db4b2f73c3d827705612356dab11ef597231f1a8
davimp3 <davi.nepomuceno@icloud.com>
2025-08-21 11:37:57 -0300

    Versão 1.6

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/davi-nepomuceno/Documents/Indicadores/venv
+set -gx VIRTUAL_ENV /home/davi-nepomuceno/Documents/Git/Indicadores/venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
